1cd990e66d686f6ae2555a5f98b8b3027e179483
Pavel <yatrushevpm@gmail.com>
2025-02-14 17:00:19 +0300

    files

--- pdp-11_mem.py
+++ pdp-11_mem.py
@@ -10,34 +10,39 @@ def b_write(adr, value):
 def b_read(adr):
     return mem[adr]
            
+
 def w_write(adr, value):
-    mem[adr+1] = hex(int(value, 16) >> 8)
-    mem[adr] = hex(int(value, 16))
-    
+    if adr % 2 != 0:
+        raise ValueError("Word adress must be even")
+    mem[adr+1] = value >> 8 & 0xFF
+    mem[adr] = value & 0xFF
+
 
 def w_read(adr):
-    word = int(mem[adr+1], 16) << 8
-    word |= int(mem[adr], 16)
-    print(hex(word))
-    return hex(word)
+    if adr % 2 != 0:
+        raise ValueError("Word adress must be even")
+    word = mem[adr+1] << 8 
+    word |= mem[adr]
+        
+    return word & 0xFFFF
 
 
 
 
-b0 = '0x0a'
-b1 = '0x0b'
+b0 = 0x0a
+b1 = 0x0b
 b_write(2, b0)
 assert b0 == b_read(2)
 
 
 adr = 4
-w = '0xb0a'
+w = 0xb0a
 b_write(adr, b0)
 b_write(adr+1, b1)
 wres = w_read(adr)    
 
 adr = 6
-word = '0x0'
+word = 0x0
 w_write(6, word)
 assert word == w_read(6)
 
@@ -45,3 +50,5 @@ assert word == w_read(6)
 print(wres, b1, b0)
 assert w == wres
 
+w_write(0, 0x1234)
+print(hex(w_read(0)))  # '0x1234'
